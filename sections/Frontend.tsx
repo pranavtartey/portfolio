0bc7c4787a7bd20cf68459f124da9983146ad6c8
Pranav Tartey <pranavtartey@gmail.com>
2024-12-29 01:32:25 +0530

    Added the frontend-card to components

--- sections/Frontend.tsx
+++ sections/Frontend.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
-import FrontendCard from "./FrontendCard";
+import FrontendCard from "../components/FrontendCard";
 import { frontendProjects } from "@/constants/frontend";
 
 const Frontend: FC = () => {
   return (
     <section className="py-[72px]">
       <div className="container">
-        <h2 className="uppercase text-center text-4xl tracking-widest font-medium mb-14">frontend</h2>
+        <h2 className="uppercase text-center text-4xl tracking-widest font-medium mb-14">
+          frontend
+        </h2>
         <div className="md:flex gap-4 max-md:space-y-4">
           {frontendProjects.map((project, idx) => (
             <FrontendCard {...project} key={idx} />
