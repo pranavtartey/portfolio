230352dd676b15f0de94a391562b93920a2e6ef9
Pranav Tartey <pranavtartey@gmail.com>
2025-01-12 22:11:47 +0530

    removed the animate pulse from exp section

--- sections/Experience.tsx
+++ sections/Experience.tsx
@@ -10,10 +10,10 @@ const Experience: FC = () => {
           experience
         </h2>
         <div className="px-6 py-4 relative overflow-hidden rounded-lg space-y-4">
-          <div className="absolute left-1/2 -top-[25%] -translate-x-1/2 h-[200px] w-[200px] md:h-[400px] md:w-[400px] rounded-[100%] bg-[radial-gradient(closest-side,rgba(255,172,109,0.8)_0%,rgba(226,189,161,0)_100%)] blur-3xl animate-pulse" />
-          <div className="absolute right-0 translate-x-1/2 w-[200px] h-[400px] md:w-[400px] md:h-[600px] rounded-[100%] bg-[radial-gradient(farthest-side,rgba(161,205,226,0.7)_100%,rgba(161,205,226,0)_0%)] blur-3xl animate-pulse" />
-          <div className="absolute -bottom-1/2 translate-y-[25%] h-[400px] w-[400px] md:h[600px] md:w-[600px] rounded-[100%] bg-[radial-gradient(closest-side,rgba(226,165,161,0.7)_0%,rgba(226,165,161,0)_100%)] blur-3xl animate-pulse" />
-          <div className="absolute left-0 -translate-x-1/2 w-[200px] h-[400px] md:w-[400px] md:h-[600px] rounded-[100%] bg-[radial-gradient(farthest-side,rgba(255,221,50,0.703)_100%,rgba(255,221,50,0.235)_0%)] blur-3xl animate-pulse" />
+          <div className="absolute left-1/2 -top-[25%] -translate-x-1/2 h-[200px] w-[200px] md:h-[400px] md:w-[400px] rounded-[100%] bg-[radial-gradient(closest-side,rgba(255,172,109,0.8)_0%,rgba(226,189,161,0)_100%)] blur-3xl mix-blend-multiply" />
+          <div className="absolute right-0 translate-x-1/2 w-[200px] h-[400px] md:w-[400px] md:h-[600px] rounded-[100%] bg-[radial-gradient(farthest-side,rgba(161,205,226,0.7)_100%,rgba(161,205,226,0)_0%)] blur-3xl mix-blend-multiply" />
+          <div className="absolute -bottom-1/2 translate-y-[25%] h-[400px] w-[400px] md:h[600px] md:w-[600px] rounded-[100%] bg-[radial-gradient(closest-side,rgba(226,165,161,0.7)_0%,rgba(226,165,161,0)_100%)] blur-3xl mix-blend-multiply" />
+          <div className="absolute left-0 -translate-x-1/2 w-[200px] h-[400px] md:w-[400px] md:h-[600px] rounded-[100%] bg-[radial-gradient(farthest-side,rgba(255,221,50,0.703)_100%,rgba(255,221,50,0.235)_0%)] blur-3xl  mix-blend-multiply" />
           {experiences.map((exp, idx) => (
             <ExperienceCard {...exp} key={idx} />
           ))}
