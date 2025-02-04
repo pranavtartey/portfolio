7b912cb7e2f1a2348c711bc9028dbfe9ad987424
Pranav Tartey <pranavtartey@gmail.com>
2025-02-05 02:05:46 +0530

    added new frontend project (Rivals)

--- constants/frontend.ts
+++ constants/frontend.ts
@@ -1,5 +1,11 @@
 export const frontendProjects = [
     {
+        title: "Rivals",
+        description: "Designed and built a web application for a gaming lounge with complex but subtle animations.",
+        live: "https://rivals-bay.vercel.app/",
+        github: "https://github.com/pranavtartey/rivals",
+    },
+    {
         title: "Personal Protfolio",
         description: "Designed and build every animation and component from scratch for unique experience.",
         live: "#",
@@ -10,11 +16,5 @@ export const frontendProjects = [
         description: "Created a sample dark ui for the landing page of a SaaS website.",
         live: "https://dark-ui-page-tpyo.vercel.app/",
         github: "https://github.com/pranavtartey/dark-ui-page",
-    },
-    {
-        title: "Light Landing Page",
-        description: "Created a sample light ui for the landing page of a SaaS website.",
-        live: "https://light-saas-landing-page-pi.vercel.app/",
-        github: "https://github.com/pranavtartey/Light_landingpage",
     },
 ]
--- sections/LogoTicker.tsx
+++ sections/LogoTicker.tsx
@@ -2,7 +2,7 @@
 import { FC } from "react";
 import { motion } from "framer-motion";
 import JavaScriptLogo from "@/assets/javascript-png.png";
-import TypeScriptLogo from "@/assets/typescript-png.png"
+import TypeScriptLogo from "@/assets/typescript-png.png";
 import NodeJsLogo from "@/assets/nodejs-png.png";
 import MongoDBLogo from "@/assets/mongodb-png.png";
 import PostmanLogo from "@/assets/postman-png.png";
@@ -18,7 +18,9 @@ import Image from "next/image";
 const LogoTicker: FC = () => {
   return (
     <section className="py-8 md:py-12 bg-stone-50">
-      <p className="font-semibold text-neutral-400 text-center mb-10">Can be trusted with these technologies</p>
+      <p className="font-semibold text-neutral-400 text-center mb-10">
+        Can be trusted with these technologies
+      </p>
       <div className="container">
         <div className="flex overflow-x-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
           <motion.div
@@ -33,30 +35,150 @@ const LogoTicker: FC = () => {
               repeatType: "loop",
             }}
           >
-            <Image src={JavaScriptLogo} alt="javascript-logo" width={70} className="w-12"/>
-            <Image src={TypeScriptLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={NodeJsLogo} alt="type-logo" width={120} className="w-24"/>
-            <Image src={MongoDBLogo} alt="type-logo" width={120} className="w-24"/>
-            <Image src={PostmanLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={ReactLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={PostgreSQLLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={NextJSLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={PrismaLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={TailwindLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={ExpressLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={DockerLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={JavaScriptLogo} alt="javascript-logo" width={70} className="w-12"/>
-            <Image src={TypeScriptLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={NodeJsLogo} alt="type-logo" width={120} className="w-24"/>
-            <Image src={MongoDBLogo} alt="type-logo" width={120} className="w-24"/>
-            <Image src={PostmanLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={ReactLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={PostgreSQLLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={NextJSLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={PrismaLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={TailwindLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={ExpressLogo} alt="type-logo" width={70} className="w-12"/>
-            <Image src={DockerLogo} alt="type-logo" width={70} className="w-12"/>
+            <Image
+              src={JavaScriptLogo}
+              alt="javascript-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={TypeScriptLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={NodeJsLogo}
+              alt="type-logo"
+              width={120}
+              className="w-24"
+            />
+            <Image
+              src={MongoDBLogo}
+              alt="type-logo"
+              width={120}
+              className="w-24"
+            />
+            <Image
+              src={PostmanLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={ReactLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={PostgreSQLLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={NextJSLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={PrismaLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={TailwindLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={ExpressLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={DockerLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={JavaScriptLogo}
+              alt="javascript-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={TypeScriptLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={NodeJsLogo}
+              alt="type-logo"
+              width={120}
+              className="w-24"
+            />
+            <Image
+              src={MongoDBLogo}
+              alt="type-logo"
+              width={120}
+              className="w-24"
+            />
+            <Image
+              src={PostmanLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={ReactLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={PostgreSQLLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={NextJSLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={PrismaLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={TailwindLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={ExpressLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
+            <Image
+              src={DockerLogo}
+              alt="type-logo"
+              width={70}
+              className="w-12"
+            />
           </motion.div>
         </div>
       </div>
